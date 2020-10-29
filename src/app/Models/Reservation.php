8a2ae97573335813f7cfbdc1187b37d8e1931c48
Tomasz Bielecki <bieleckitomasz94@gmail.com>
2020-10-29 15:49:49 +0100

    ADD: Databases components
    -ADD: migrations (users, cars, reservations)
    
    ADD: models (user, car, reservation)
    
    ADD: seeders (users, cars, reservationss)
    
    ADD: factories (user, car, reservation)

---
+++ src/app/Models/Reservation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'car_id',
+        'start_date',
+        'finish_date',
+        'cancelled_at',
+    ];
+}
